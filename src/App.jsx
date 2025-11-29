7094e6543db721c97c633bca7bc05161b87edf56
Amarjeet <amarjeetmistri41@gmail.com>
2025-11-29 11:47:32 +0530

    thoda change

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,22 @@ import Contact from './pages/Contact'
 import { Toaster } from 'react-hot-toast'
 import Works from './pages/Works'
 import Clock from './components/Clock'
+import myResume from './assets/Resume.pdf'
 
 function App() {
   return (
-    <div className='font-[JetBrains_Mono] container mx-auto'>
+    <div className='font-[JetBrains_Mono] container mx-auto '>
       <Header />
       <main className=' w-full md:w-[90%] lg:w-[80%] xl:[70%] mx-auto  '>
         <Routes>
           <Route path='/' element={<Dashboard />} />
           <Route path='/resume' element={<Resume />} />
+          {/* <Route path='/resume/myResume' element={myResume} /> */}
           <Route path='/works' element={<Works/> } />
           <Route path='/contact' element={<Contact />} />
-          <Route path='/clock' element={<Clock/>}/>
+          <Route path='/clock' element={<Clock />} />
+          <Route path='*' element={<Dashboard />} />
+
           
         </Routes>
       </main>
